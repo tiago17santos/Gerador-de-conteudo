64ff7e9daee63dba5cea3d6ce0a6f47a1fa3e33e
Tiago Santos <tiagoalbertoo17@gmail.com>
2024-10-13 17:33:07 -0300

    Criando arquivo fxml com a interface do sistema

---
+++ Gerador de conteudo/src/main/java/com/ic/geradordeconteudo/GeradorConteudoController.java
@@ -0,0 +1,25 @@
+package com.ic.geradordeconteudo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+
+public class GeradorConteudoController {
+
+    @FXML
+    private Button btnGerar;
+
+    @FXML
+    private Pane pane;
+
+    @FXML
+    private TextField txtConteudo;
+
+    @FXML
+    void onClickGerar(ActionEvent event) {
+
+    }
+
+}
--- Gerador de conteudo/src/main/java/com/ic/geradordeconteudo/HelloApplication.java
+++ Gerador de conteudo/src/main/java/com/ic/geradordeconteudo/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("gerador-conteudo.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800,600);
+        stage.setTitle("Gerador de Conteúdo");
         stage.setScene(scene);
         stage.show();
     }
