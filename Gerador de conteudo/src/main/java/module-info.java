163aa279c28543886466a872fde8b24cef7348ee
Tiago Santos <67645178+tiago17santos@users.noreply.github.com>
2025-01-29 13:57:22 -0300

    Delete Gerador de conteudo directory

--- Gerador de conteudo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.ic.geradordeconteudo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ic.geradordeconteudo to javafx.fxml;
-    exports com.ic.geradordeconteudo;
-}
